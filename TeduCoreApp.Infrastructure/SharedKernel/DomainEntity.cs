7d5b125c0a704346945a02b4c3f203e85715c6ab
PhuongDong <phuongdong2097@gmail.com>
2022-02-07 00:38:25 +0700

    lesson 11, xây dựng các entity

---
+++ TeduCoreApp.Infrastructure/SharedKernel/DomainEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace TeduCoreApp.Infrastructure.SharedKernel
+{
+    public abstract class DomainEntity<T>
+    {
+        public T Id { get; set; }
+
+        public bool IsTransient()
+        {
+            //True: nếu mới tạo ra chưa set giá trị
+            //false: đã set giá trị
+            return Id.Equals(default(T));
+        }
+    }
+}
