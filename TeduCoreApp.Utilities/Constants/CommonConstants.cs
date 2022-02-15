bbe17637fccb078d0e4f6d822948fbb644d68a2e
PhuongDong <phuongdong2097@gmail.com>
2022-02-15 22:38:07 +0700

    lesson 29: thêm sửa xóa product admin

--- TeduCoreApp.Utilities/Constants/CommonConstants.cs
+++ TeduCoreApp.Utilities/Constants/CommonConstants.cs
@@ -7,6 +7,10 @@ namespace TeduCoreApp.Utilities.Constants
     public class CommonConstants
     {
         public const string DefaultFooterId = "DefaultFooterId";
+
         public const string AdminRole = "Admin";
+
+        public const string ProductTag = "Product";
+        public const string BlogTag = "Blog";
     }
 }
